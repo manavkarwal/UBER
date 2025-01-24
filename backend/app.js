ddf00612fa76e240f302bb637aee00dcd29a2ef2
Manav karwal <haigamekhelte@gmail.com>
2025-01-24 17:25:54 +0530

    create profile route

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,21 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const express = require('express');
-const cors = require('cors');
-const connectToDb = require('./db/db');
-const userRoutes = require('./routes/user.routes')
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+const cookieParser = require("cookie-parser");
 const app = express();
-app.use(cors())
+app.use(cors());
 connectToDb();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('hello world')
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
-app.use('/users', userRoutes);
-
+app.use("/users", userRoutes);
 
 module.exports = app;
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const userController = require('../controllers/user.controllers'); 
+const authMiddleware = require('../middlewares/auth.middleware')
 
 router.post("/register", [
   body("email").isEmail().withMessage("Invalid Email"),
@@ -20,5 +21,8 @@ router.post("/login", [
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 character long"),
 ] , userController.loginUser);
+
+
+router.get("/profile", authMiddleware.authUser ,  userController.getUserProfile)
 
 module.exports = router;
